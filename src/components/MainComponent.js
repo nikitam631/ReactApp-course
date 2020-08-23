9c84aebdd3e702750611358a136de2a8b02d9915
nikitam631 <nikita232806@gmail.com>
2020-08-23 16:34:23 +0530

    react router-home,menu, toggle method bind in header

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,31 +4,44 @@ import DishDetail from './DishDetailComponent';
 import {DISHES} from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props){
     super(props)
     this.state ={
         dishes:DISHES,
-        selectedDish: null
+        //selectedDish: null
     }
   }
-  onDishSelect(dishId){
+  /*onDishSelect(dishId){
     
     this.setState({
         selectedDish : dishId
     })
     
-  };
+  };*/
  render(){
+  
+  const HomePage = () =>{
+    return(
+      <Home/>
+    )
+  }
   return (
     <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Switch>
+          <Route path='/home' component={HomePage}></Route>
+          <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}></Menu>}></Route>
+          <Redirect to='/home' />
+        </Switch>
+        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail
              dish={this.state.dishes.filter((dish)=>
                 dish.id === this.state.selectedDish
-             )[0]}></DishDetail>
+             )[0]}></DishDetail> */}
         <Footer />
       </div>
   );
