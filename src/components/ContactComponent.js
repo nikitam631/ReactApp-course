1b7bf56336226f5ae3b8158abc1070f607713983
nikitam631 <nikita232806@gmail.com>
2020-09-02 07:32:40 +0530

    form state save to store

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Label, Col, Button, Row } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -18,6 +18,7 @@ class Contact extends Component {
     handleSubmit(values){
         console.log(JSON.stringify(values));
         alert(JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
     render(){
@@ -54,7 +55,7 @@ class Contact extends Component {
                         <h3>Send us your feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
-                        <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={(values)=>this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -167,7 +168,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import About from './AboutComponent';
 
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { addComment, fetchDishes } from '../redux/ActionCreator';
+import {actions} from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -22,7 +23,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment)),
-  fetchDishes: () => dispatch(fetchDishes())
+  fetchDishes: () => dispatch(fetchDishes()),
+  resetFeedbackForm: () => dispatch(actions.reset('feedback'))
 });
 
 class Main extends Component {
@@ -66,7 +68,7 @@ class Main extends Component {
           <Route path='/home' component={HomePage}></Route>
           <Route exact path='/menu' component={()=><Menu dishes={this.props.dishes}></Menu>}></Route>
           <Route path='/menu/:dishId' component={DishWithId}></Route>
-          <Route exact path='/contactus' component={Contact}></Route>
+          <Route exact path='/contactus' component={()=><Contact resetFeedbackForm={this.props.resetFeedbackForm} />}></Route>
           <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders}></About>}></Route>
           <Redirect to='/home' />
         </Switch>
